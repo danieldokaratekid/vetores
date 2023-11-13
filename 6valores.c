73616ee74a3169096c9b04790d8e8c8655c04d81
danieldokaratekid <143547688+danieldokaratekid@users.noreply.github.com>
2023-11-13 16:56:29 -0300

    Add files via upload

---
+++ 6valores.c
@@ -0,0 +1,12 @@
+//questão 4
+//crie um programa que leia 6 valores inteiros pares 
+//e em seguida monstre na tela os valores lidos na ordem inversa
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+int i;
+int valores;
+printf("Digite um valor:");
+scanf("%d",&valores);
+
+}
