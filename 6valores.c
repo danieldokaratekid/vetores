09415a0e28e891073f458a353d42936d8dbaa729
danieldokaratekid <143547688+danieldokaratekid@users.noreply.github.com>
2023-11-13 23:21:26 -0300

    Update 6valores.c

--- 6valores.c
+++ 6valores.c
@@ -1,12 +1,32 @@
-//quest�o 4
-//crie um programa que leia 6 valores inteiros pares 
-//e em seguida monstre na tela os valores lidos na ordem inversa
-#include<stdio.h>
-#include<stdlib.h>
-int main(){
-int i;
-int valores;
-printf("Digite um valor:");
-scanf("%d",&valores);
+// questão 4
+// crie um programa que leia 6 valores inteiros pares
+// e em seguida monstre na tela os valores lidos na ordem inversa
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int i;
+    int valores[6];
+    int inverso[6];
+    int pares;
+    for (i = 0; i < 6; i++)
+    {
+        printf("Digite um valor:");
+        scanf("%d", &valores[i]);
+        if (valores[i] % 2 == 0)
+        {
+            printf("\n%d é par\n", inverso[i]);
+            pares++;
+        }
+            
+        }
+        for (i = 6; i >=0; i--)
+        {
+            printf("%.2f", inverso[i]);
+            printf("\nexibindo os numeros....\n");
 
-}
+            printf("%d,inverso:%d\n",inverso[i]);
+        }
+        printf("\n pares:%d\n", pares);
+        return 0;
+    }
