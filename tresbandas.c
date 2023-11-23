0dffd442832806b3b6cf06ccbc14f52461e2f6b9
danieldokaratekid <143547688+danieldokaratekid@users.noreply.github.com>
2023-11-23 20:31:11 -0300

    Update tresbandas.c

--- tresbandas.c
+++ tresbandas.c
@@ -7,9 +7,18 @@ int main(){
 int i,j;
 char banda[200][3];
 char integrantes[200][5];
+for (i = 0; i < 3; i++)
+{
 printf("Digite uma banda:\n");
 gets(banda);
+}
+for (i = 0; i < 5; i++)
+{
 printf("Digite 5 integrantes da banda\n");
 gets(integrantes);
+}
+printf("\nbandas:%s\n",&banda[i]);{
+printf("\nintegrantes:%s\n",&integrantes);
+}
 return 0;
 }
