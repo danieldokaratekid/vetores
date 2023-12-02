475d4d0ab0280049b8b99acb47b099bbce927d9d
danieldokaratekid <143547688+danieldokaratekid@users.noreply.github.com>
2023-12-02 16:22:05 -0300

    Update funcaotabuada.c

--- funcaotabuada.c
+++ funcaotabuada.c
@@ -1,18 +1,19 @@
-//quest„o 2:
-//FaÁa um programa que leia uma tabela com a tabuada de um numero informado pelo usuario 1 a 10;
+//quest√£o 2:
+//Fa√ßa um programa que leia uma tabela com a tabuada de um numero informado pelo usuario 1 a 10;
 #include<stdio.h>
 #include<stdlib.h>
-void mostrartabuada(int numero){
+void tabuada(int numero){
 int i;
 for ( i = 1; i < 10; i++)
 {
 printf("%d x %d =%d\n",numero,i,numero*i);
 }
 }
-int main(){
+int main()
+{
 int numero;
 printf("Digite um numero:");
 scanf("%d",&numero);
-monstrartabuada(numero);
+tabuada(numero);
 return 0;
 }
